9b88552334c3c9e0c0214b4c4ec992f9f2c64bce
corybacchetta <corybacchetta5@gmail.com>
2025-06-18 11:21:32 -0500

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-const CACHE_NAME = 'shop-a-rone-cache-v1';
-const ASSETS = [
-  './',
-  'index.html','style.css','script.js','shop-a-rone-logo.png','manifest.json'
-];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
